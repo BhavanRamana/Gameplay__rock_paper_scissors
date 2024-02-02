939d41c237c1d858eb7f35b050ecff6b445c5256
BhavanRamana <bhavanramana2002@gmail.com>
2024-02-02 18:50:41 +0530

    Initial commit

---
+++ Gameplay.py
@@ -0,0 +1,28 @@
+import random
+def gameplay():
+    user = input("what's your choice? 'r' for rock,'p' for paper 's' for scissors:")
+    computer = random.choice(["r","p","s"])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    if winner(user,computer):
+        return 'You won!'
+
+    return 'you lost!'
+
+def winner(player,opponent):
+    if (player == 'r' and opponent == 'p') or (player == 'p' and opponent == 's')\
+            or ( player == 's' and opponent == 'r'):
+        return True
+
+print(gameplay())
+
+
+
+
+
+
+
+
+
